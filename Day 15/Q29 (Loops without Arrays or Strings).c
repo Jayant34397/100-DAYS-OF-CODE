c9e1df3d61d8ab6388e991f1ba3354e36625c47c
Jayant34397 <JAYANT.34397@stu.upes.ac.in>
2026-08-24 12:00:00 +0530

    Day 15: Q29 (Loops without Arrays or Strings)

---
+++ Day 15/Q29 (Loops without Arrays or Strings).c
@@ -0,0 +1,34 @@
+// Q29: Write a program to calculate the factorial of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+120
+
+Input 2:
+3
+Output 2:
+6
+
+*/
+
+#include <stdio.h>
+int main() {
+    int n;
+    int factorial = 1;
+    int num = 1;
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    while (num <= n) {
+        factorial *= num;
+        num++;
+    }
+    
+    printf("Factorial of %d is: %d\n", n, factorial);
+    
+    return 0;
+}
