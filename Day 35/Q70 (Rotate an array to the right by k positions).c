a078c444e96b323f6abe5b20c7207dad575a9802
Jayant34397 <JAYANT.34397@stu.upes.ac.in>
2026-09-13 12:05:00 +0530

    Day 35: Q70 (Rotate an array to the right by k positions)

---
+++ Day 35/Q70 (Rotate an array to the right by k positions).c
@@ -0,0 +1,26 @@
+// Q70: Rotate an array to the right by k positions.
+/*
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+2
+Output 1:
+4 5 1 2 3
+*/
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[100];
+    for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+    int k;
+    scanf("%d", &k);
+    k = k % n;
+    int temp[100];
+    for(int i=0; i<k; i++) temp[i] = arr[n-k+i];
+    for(int i=n-1; i>=k; i--) arr[i] = arr[i-k];
+    for(int i=0; i<k; i++) arr[i] = temp[i];
+    for(int i=0; i<n; i++) printf("%d%c", arr[i], i == n-1 ? '\n' : ' ');
+    return 0;
+}
