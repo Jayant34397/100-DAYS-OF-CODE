dab5483b36a31030c8877c683c1c745f032790e8
Jayant Gupta <JAYANT.34397@stu.upes.ac.in>
2026-09-15 10:18:37 +0530

    Add files via upload

---
+++ DAY37QUES73.c
@@ -0,0 +1,25 @@
+/*Find the sum of each row of a matrix and store it in an array.*/
+
+#include <stdio.h>
+int main(){
+    int a[3][3], sum[3] = {0};
+    int i, j;
+
+    printf("Enter 9 elements:\n");
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+ scanf("%d", &a[i][j]);
+ sum[i] = sum[i] + a[i][j];
+        }
+    }
+    printf("Sum of each row:\n");
+
+    for(i = 0; i < 3; i++)
+    {
+        printf("Row %d = %d\n", i + 1, sum[i]);
+    }
+
+    return 0;
+}
