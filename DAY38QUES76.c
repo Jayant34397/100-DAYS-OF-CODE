5085712740c90d875d10d5e90dbb8c017389e2b6
Jayant Gupta <JAYANT.34397@stu.upes.ac.in>
2026-09-16 19:35:30 +0530

    Add files via upload

---
+++ DAY38QUES76.c
@@ -0,0 +1,29 @@
+/*Check if a matrix is symmetric.*/
+
+#include <stdio.h>
+int main(){
+    int a[2][2], i, j, symmetric = 1;
+    printf("Enter elements of matrix:\n");
+  for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 2; j++)
+        {
+            if(a[i][j] != a[j][i])
+            {
+                symmetric = 0;
+            }
+        }
+    }
+         if(symmetric == 1)
+        printf("Matrix is symmetric");
+    else
+        printf("Matrix is not symmetric");
+    return 0;
+}
