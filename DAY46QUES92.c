79db463b9eb85f83cbbc53875506a3c94fb3f608
Jayant Gupta <JAYANT.34397@stu.upes.ac.in>
2026-09-24 11:15:40 +0530

    Add files via upload

---
+++ DAY46QUES92.c
@@ -0,0 +1,23 @@
+/*
+Find the first repeating lowercase alphabet in a string.*/
+
+#include <stdio.h>
+int main(){
+    char str[100];
+    int i, j;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        for(j = i + 1; str[j] != '\0'; j++) {
+            if(str[i] == str[j]) {
+                printf("First repeating character = %c", str[i]);
+                return 0;
+            }
+        }
+    }
+    printf("No repeating character");
+
+    return 0;
+}
